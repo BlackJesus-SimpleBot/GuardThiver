600b0df20230af292b2d09303005caad2f48ca8c
BlackJesus-SimpleBot <58403700+BlackJesus-SimpleBot@users.noreply.github.com>
2019-12-04 17:18:54 -0500

    Update StealTask.java

--- src/StealTask.java
+++ src/StealTask.java
@@ -5,21 +5,20 @@ import simple.hooks.wrappers.SimpleItem;
 import simple.hooks.wrappers.SimpleNpc;
 import simple.robot.api.ClientContext;
 
-
 public class StealTask extends Task {
 
-    public StealTask(ClientContext ctx) {
-        super(ctx);
-    }
+	public StealTask(ClientContext ctx) {
+		super(ctx);
+	}
 
-    @Override
-    public boolean condition() {
-        return ctx.getPlayers().getLocal().getHealth() >= 10;
-    }
+	@Override
+	public boolean condition() {
+		return ctx.players.getLocal().getHealth() >= 9;
+	}
 
-    @Override
-    public void run() {
-    	int coinPouchCount = ctx.inventory.populate().filter(22526).population(true);
+	@Override
+	public void run() {
+		int coinPouchCount = ctx.inventory.populate().filter(22526).population(true);
 
 		if (coinPouchCount >= 27) {
 			SimpleItem coinPouch = ctx.inventory.populate().filter(22526).next();
@@ -30,33 +29,35 @@ public class StealTask extends Task {
 			}
 		}
 
-		if (ctx.getPlayers().getLocal().getHealth() < 9) {
+		if (ctx.combat.health() <= 9) {
 			SimpleItem Lobster = ctx.inventory.populate().filter(379).next();
 			if (Lobster != null) {
 				if (Lobster.click("Eat")) {
-					ctx.sleepCondition(() -> ctx.getPlayers().getLocal().getHealth() > 9, 2000);
+					ctx.sleepCondition(() -> ctx.combat.health() > 9, 2000);
 
 				}
 			}
 		} else {
-			if (ctx.getPlayers().getLocal().getAnimation() == 424) {
-				ctx.sleepCondition(() -> ctx.getPlayers().getLocal().getAnimation() != 424, 2500);
+			if (ctx.players.getLocal().getAnimation() == 424) {
+				ctx.sleepCondition(() -> ctx.players.getLocal().getAnimation() != 424, 2500);
 			} else {
-				SimpleNpc Guard = ctx.getNpcs().populate().filter("Guard").nextNearest();
+				ctx.sleep(1000);
+			SimpleNpc Guard = ctx.npcs.populate().filter("Guard").nextNearest();
 				if (Guard != null) {
 					if (Guard.validateInteractable()) {
 						if (Guard.click("Pickpocket")) {
-							ctx.sleepCondition(() -> ctx.getPlayers().getLocal().getAnimation() != -1, 500);
+							ctx.sleepCondition(() -> ctx.players.getLocal().getAnimation() != -1, 1000);
 						}
 					}
 				}
 			}
 		}
-		ctx.sleep(200, 400);
+		ctx.sleep(200, 1000);
 	}
-    @Override
-    public String status() {
-        return "Stealing";
-    }
+
+	@Override
+	public String status() {
+		return "Stealing";
+	}
 
 }
